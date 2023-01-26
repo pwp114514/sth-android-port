5180dbf1f7c8b5d762c4c1ed15e0ce8abef15dfb
fnf small porter <117057486+Liulw123@users.noreply.github.com>
2023-01-26 10:14:14 +0800

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2360,7 +2360,7 @@ class PlayState extends MusicBeatState
 				}
 
 				var introAssets:Map<String, Array<String>> = new Map<String, Array<String>>();
-				introAssets.set('default', ['ready', 'set', 'go']);
+				introAssets.set('default', ['dont','you', 'believe','it']);
 				introAssets.set('pixel', ['pixelUI/ready-pixel', 'pixelUI/set-pixel', 'pixelUI/date-pixel']);
 
 				var introAlts:Array<String> = introAssets.get('default');
